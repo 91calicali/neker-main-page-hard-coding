5d5ee06715653568446273564ad309986cbe3e23
91calicali <91calicali@gmail.com>
2024-01-12 18:34:48 +0900

    Neker first commit

---
+++ js/tmpdata.js
@@ -0,0 +1,58 @@
+const pds = {
+    p1: {
+        img: "resource/image/ap_image1.png",
+        name: "베일레이어 매트 립",
+        price: "19,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p2: {
+        img: "resource/image/ap_image2.png",
+        name: "베일레이어 치크",
+        price: "17,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p3: {
+        img: "resource/image/ap_image3.png",
+        name: "컬러스웨이드 립라커",
+        price: "18,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p4: {
+        img: "resource/image/ap_image4.png",
+        name: "베일레이어 섀도우 팔레트",
+        price: "22,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p5: {
+        img: "resource/image/ap_image5.png",
+        name: "프리즈매틱 컬러 아이즈",
+        price: "12,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p6: {
+        img: "resource/image/ap_image6.png",
+        name: "드로잉 에디터 아이라이너 펜슬",
+        price: "12,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p7: {
+        img: "resource/image/ap_image7.png",
+        name: "모이스터치 아이 크림",
+        price: "29,000원",
+        soldout: 1,
+        href: "#"
+    },
+    p8: {
+        img: "resource/image/ap_image8.png",
+        name: "[네케르 바이룰] FX 비비",
+        price: "21,000원",
+        soldout: 1,
+        href: "#"
+    }
+};
